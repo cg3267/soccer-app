d24eb25d785fbff519e43d067e85cf56646a65d9
cg3267 <cg3267@g.rit.edu>
2025-01-06 01:51:11 -0500

    import

--- soccer-server/server.js
+++ soccer-server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors'); 
 const app = express();
+const path = require('path');
+
 const port = process.env.PORT || 5005;
 
 app.use(cors());
