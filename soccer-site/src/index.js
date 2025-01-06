fa94bcb47c87eca7559db2456986ebffc7ebd969
cg3267 <cg3267@g.rit.edu>
2025-01-06 01:24:31 -0500

    import

--- soccer-site/src/index.js
+++ soccer-site/src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client';  // For React 18
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';  // Import BrowserRouter
-import reportWebVitals from './reportWebVitals';
+import { reportWebVitals } from 'web-vitals';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
